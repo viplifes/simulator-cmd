3d0dc0b622e114093b7ab0133b21b29149ff8b09
Dmitry Goncharov <dmitriy.goncharov@corezoid.com>
2022-11-29 16:14:09 +0200

    added voronoi diagram

--- command/voronoi.go
+++ command/voronoi.go
@@ -5,15 +5,13 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
 	"image/png"
-	"io"
 	"log"
-	"os"
-	"text/template"
 
-	"github.com/srwiley/oksvg"
-	"github.com/srwiley/rasterx"
-	"github.com/viplifes/simulator-cmd/command/voronoi"
+	"github.com/llgcode/draw2d/draw2dimg"
+
+	"github.com/pzsz/voronoi"
 	"github.com/viplifes/simulator-cmd/entity"
 	"github.com/viplifes/simulator-cmd/simulator"
 )
@@ -38,8 +36,13 @@ func VoronoiAdd(data map[string]interface{}) (string, error) {
 	var graph entity.LayerActors
 	entity.Decode(resp, &graph)
 
-	maxX := 1000.0
-	maxY := 1000.0
+	lineWidth := 2.0
+	overflow := 100.0
+	fixMinusX := 0.0
+	fixMinusY := 0.0
+
+	maxX := 0.0
+	maxY := 0.0
 
 	var nodes []entity.Actor
 
@@ -54,42 +57,71 @@ func VoronoiAdd(data map[string]interface{}) (string, error) {
 	}
 
 	for _, v := range nodes {
-		if v.Position.X > maxX {
+
+		// fix
+		if v.Position.X < fixMinusX {
+			fixMinusX = v.Position.X
+		}
+		if v.Position.Y < fixMinusY {
+			fixMinusY = v.Position.Y
+		}
+
+		//x y
+		if v.Position.X > maxX || maxX == 0.0 {
 			maxX = v.Position.X
 		}
-		if v.Position.Y > maxY {
+
+		if v.Position.Y > maxY || maxY == 0.0 {
 			maxY = v.Position.Y
 		}
 	}
 
-	vor := voronoi.Voronoi{}
-	svg := SVG{
-		Title:       "Voronoi diagram",
-		Description: "Edges and points",
-		Width:       maxX,
-		Height:      maxY,
-		StrokeWidth: 3,
-		PointRadius: 5,
-		Vertices:    make([]*voronoi.Point, len(nodes)),
+	if fixMinusX < 0 {
+		fixMinusX = fixMinusX - overflow
 	}
-	for i, v := range nodes {
-		svg.Vertices[i] = voronoi.Pt(v.Position.X, v.Position.Y)
-	}
-	svg.Edges = vor.GetEdges(&svg.Vertices, svg.Width, svg.Height)
-	tmpl := template.Must(template.New("svg").Parse(TEMPLATE))
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, svg); err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
+	if fixMinusY < 0 {
+		fixMinusY = fixMinusY - overflow
 	}
 
-	////// DELETE PREV ACTOR
-	client.Delete("actors/ref/"+formId+"/"+voronoiNodeRef, nil, nil)
+	maxX = maxX + overflow - fixMinusX
+	maxY = maxY + overflow - fixMinusY
 
-	////// UPLOAD FILE
+	bbox := voronoi.NewBBox(0, maxX, 0, maxY)
+	sites := make([]voronoi.Vertex, len(nodes))
+	for j := 0; j < len(nodes); j++ {
+		sites[j].X = nodes[j].Position.X - fixMinusX
+		sites[j].Y = nodes[j].Position.Y - fixMinusY
+	}
+
+	diagram := voronoi.ComputeDiagram(sites, bbox, false)
+
+	img := image.NewRGBA(image.Rect(0, 0, int(maxX)+int(lineWidth), int(maxY)+int(lineWidth)))
+
+	l := draw2dimg.NewGraphicContext(img)
+	l.SetLineWidth(lineWidth)
+
+	// Iterate over cells
+	for _, cell := range diagram.Cells {
+		l.SetFillColor(color.Transparent)
+		l.SetStrokeColor(color.RGBA{0xFF, 0x66, 0x00, 0xFF})
+		for _, hedge := range cell.Halfedges {
+			a := hedge.GetStartpoint()
+			b := hedge.GetEndpoint()
+			l.MoveTo(a.X, a.Y)
+			l.LineTo(b.X, b.Y)
+		}
+		l.FillStroke()
+	}
+
+	l.Close()
+
 	var b bytes.Buffer
 	foo := bufio.NewWriter(&b)
-	sVGtoPNG(buf.Bytes(), foo)
+
+	err = png.Encode(foo, img)
+	if err != nil {
+		log.Println(err)
+	}
 
 	resp, _ = client.Upload("upload/"+simulatorAccId, b.Bytes(), nil)
 
@@ -113,8 +145,8 @@ func VoronoiAdd(data map[string]interface{}) (string, error) {
 			"id":   actor.Data.Id,
 			"type": "node",
 			"position": simulator.Request{
-				"x": maxX / 2,
-				"y": maxY / 2,
+				"x": ((maxX + lineWidth*2) / 2) + fixMinusX,
+				"y": ((maxY + lineWidth*2) / 2) + fixMinusY,
 			}}},
 	}, nil)
 
@@ -152,42 +184,4 @@ func VoronoiRemove(data map[string]interface{}) (string, error) {
 	}, nil)
 
 	return fmt.Sprintf("[ok] removed Voronoi diagram"), nil
-}
-
-const TEMPLATE = `<?xml version="1.0" ?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="{{.Width}}px" height="{{.Height}}px" viewBox="0 0 {{.Width}} {{.Height}}"
-     xmlns="http://www.w3.org/2000/svg" version="1.1">
-  <title>{{.Title}}</title>
-  <desc>{{.Description}}</desc>
-  <!-- Edges -->
-  <g stroke="red" stroke-width="{{.StrokeWidth}}" fill="none">
-    {{range .Edges}}<path d="M{{.Start.X}},{{.Start.Y}} L{{.End.X}},{{.End.Y}}" />
-    {{end}}</g>
-</svg>`
-
-type SVG struct {
-	Width       float64
-	Height      float64
-	Edges       voronoi.Edges
-	Vertices    voronoi.Vertices
-	Title       string
-	Description string
-	StrokeWidth float64
-	PointRadius float64
-}
-
-func sVGtoPNG(svg []byte, pngWriter io.Writer) {
-	icon, _ := oksvg.ReadIconStream(bytes.NewBuffer(svg))
-	w := int(icon.ViewBox.W)
-	h := int(icon.ViewBox.H)
-	icon.SetTarget(0, 0, float64(w), float64(h))
-	rgba := image.NewRGBA(image.Rect(0, 0, w, h))
-	icon.Draw(rasterx.NewDasher(w, h, rasterx.NewScannerGV(w, h, rgba, rgba.Bounds())), 1)
-
-	err := png.Encode(pngWriter, rgba)
-	if err != nil {
-		log.Println(err)
-	}
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,12 @@ go 1.18
 require (
 	github.com/AvraamMavridis/randomcolor v0.0.0-20180822172341-208aff70bf2c
 	github.com/corezoid/gitcall-go-runner v1.0.0
+	github.com/llgcode/draw2d v0.0.0-20210904075650-80aa0a2a901d
 	github.com/mitchellh/mapstructure v1.5.0
-	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c
-	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780
+	github.com/pzsz/voronoi v0.0.0-20130609164533-4314be88c79f
 )
 
 require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
-	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
-	golang.org/x/text v0.3.6 // indirect
 )
